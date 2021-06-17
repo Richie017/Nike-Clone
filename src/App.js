ef8f92fc9037873c4974cdf6316e320de871bb87
Rasheeq Zaman <zamanrshq@gmail.com>
2021-06-17 11:08:02 +0600

    remove: unused files and code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
-import Topnavbar from './components/header/navbar/Topnavbar';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <React.Fragment>
-          <Topnavbar />
-    </React.Fragment> 
-  );
+import Topnavbar from './components/header/navbar/Topnavbar';
+import Footer from './components/footer';
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Topnavbar />
+                <Footer />
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
