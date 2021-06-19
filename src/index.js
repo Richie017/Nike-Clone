ecb8abc15750b6958d0a29963df2434f22dfceda
Rasheeq Zaman <zamanrshq@gmail.com>
2021-06-19 14:12:35 +0600

    add: sub footer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
-=======
->>>>>>> remove: unused files and code
 
 ReactDOM.render(
   <React.StrictMode>
