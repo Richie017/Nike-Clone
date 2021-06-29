796dafb24ac7b3a8e80d3866e3eba1a412bdbee0
Syed Salvi Md Farhavi <salvifaravi111@gmail.com>
2021-06-29 20:17:22 +0600

    update: horizontal menu items with basic hover property

--- src/components/header/navbar/Topnavbar.jsx
+++ src/components/header/navbar/Topnavbar.jsx
@@ -27,22 +27,22 @@ class Topnavbar extends Component {
           url: "/new",
           subCategory: [
             {
-              id: 1.1,
+              id: 1,
               title: "New & Featured",
               url: "/#"
             },
             {
-              id: 1.2,
+              id: 2,
               title: "New For Men",
               url: "/#"
             },
             {
-              id: 1.3,
+              id: 3,
               title: "New For Women",
               url: "/#"
             },
             {
-              id: 1.4,
+              id: 4,
               title: "New For Kids",
               url: "/#"
             }
@@ -54,27 +54,27 @@ class Topnavbar extends Component {
           url: "/men",
           subCategory: [
             {
-              id: 2.1,
+              id: 1,
               title: "New & Featured",
               url: "/#"
             },
             {
-              id: 2.2,
+              id: 2,
               title: "Shoes",
               url: "/#"
             },
             {
-              id: 2.3,
+              id: 3,
               title: "Clothing",
               url: "/#"
             },
             {
-              id: 2.4,
+              id: 4,
               title: "Accessories & Equipment",
               url: "/#"
             },
             {
-              id: 2.5,
+              id: 5,
               title: "Shop Collection",
               url: "/#"
             }
@@ -86,27 +86,27 @@ class Topnavbar extends Component {
           url: "/women",
           subCategory: [
             {
-              id: 3.1,
+              id: 1,
               title: "New & Featured",
               url: "/#"
             },
             {
-              id: 3.2,
+              id: 2,
               title: "Shoes",
               url: "/#"
             },
             {
-              id: 3.3,
+              id: 3,
               title: "Clothing",
               url: "/#"
             },
             {
-              id: 3.4,
+              id: 4,
               title: "Accessories & Equipment",
               url: "/#"
             },
             {
-              id: 3.5,
+              id: 5,
               title: "Shop Collection",
               url: "/#"
             }
@@ -118,37 +118,37 @@ class Topnavbar extends Component {
           url: "/kids",
           subCategory: [
             {
-              id: 4.1,
+              id: 1,
               title: "New & Featured",
               url: "/#"
             },
             {
-              id: 4.2,
+              id: 2,
               title: "Boys Shoes",
               url: "/#"
             },
             {
-              id: 4.3,
+              id: 3,
               title: "Girls Shoes",
               url: "/#"
             },
             {
-              id: 4.4,
+              id: 4,
               title: "Boys Clothing",
               url: "/#"
             },
             {
-              id: 4.5,
+              id: 5,
               title: "Girls Clothing",
               url: "/#"
             },
             {
-              id: 4.6,
+              id: 6,
               title: "Baby & Toddler",
               url: "/#"
             },
             {
-              id: 4.7,
+              id: 7,
               title: "Accessories & Equipment",
               url: "/#"
             }
@@ -160,17 +160,17 @@ class Topnavbar extends Component {
           url: "/custom",
           subCategory: [
             {
-              id: 5.1,
+              id: 1,
               title: "New & Featured",
               url: "/#"
             },
             {
-              id: 5.2,
+              id: 2,
               title: "Mens",
               url: "/#"
             },
             {
-              id: 5.3,
+              id: 3,
               title: "Womens",
               url: "/#"
             }
@@ -182,22 +182,22 @@ class Topnavbar extends Component {
           url: "/sale",
           subCategory: [
             {
-              id: 6.1,
+              id: 1,
               title: "Sale",
               url: "/#"
             },
             {
-              id: 6.2,
+              id: 2,
               title: "Mens Sale",
               url: "/#"
             },
             {
-              id: 6.3,
+              id: 3,
               title: "Womens Sale",
               url: "/#"
             },
             {
-              id: 6.4,
+              id: 4,
               title: "Kids Sale",
               url: "/#"
             }
@@ -233,17 +233,22 @@ class Topnavbar extends Component {
             }}
           >
             {this.state.catagories.map((category) => (
-              <Tab
-                className="Topnavbar"
-                key={category.id}
-                is="a"
-                href="#"
-              >
-                {category.title}
-              </Tab>
-              {this.catagories.subCategory.map(subLink => <a key={subLink.id} className="footerLink" href={subLink.url}>{subLink.title}</a>)}
+                <Pane className="subnav">
+                  <Tab
+                    className="Topnavbar"
+                    key={category.id}
+                    is="a"
+                    href="#"
+                  >
+                    {category.title}
+                  </Tab>
+                  <Pane className="subnav-content">
+                    {category.subCategory.map(subLink => <Tab className="commonSubCategory" key={subLink.id} is="a" href={subLink.url}>{subLink.title}</Tab>)}
+                  </Pane>
+                </Pane>
+              
             ))}
-            <Pane className="commonSubCategory">
+           {/* <Pane className="commonSubCategory">
               {this.state.catagories.map((cat) => (
                 <Pane key={cat.id}>
                   {cat.subCategory.map((subcat) => (
@@ -252,9 +257,10 @@ class Topnavbar extends Component {
                 </Pane>
               ))}
             </Pane>
+            */}
           </TabNavigation>
         </Pane>
-
+                  
         {/*Seachbar, bookmarkbtn, cartbtn*/}
         <Pane>
           <SearchInput placeholder="SEARCH" width="200px" />
