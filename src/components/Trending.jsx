1817bc2cf6b77e45bcd986cbe363c6977e06dfa8
IronManBD <nahidul19967@gmail.com>
2021-06-30 22:56:35 +0600

    add:dynamicity

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,52 +1,51 @@
-import React, { Component } from 'react';
-import { Pane, Text, Button, Heading } from 'evergreen-ui';
-import shoe1 from '../assets/images/shoe-1.png';
-import liverpool from '../assets/images/liverpool-1.png';
-import '../assets/css/ImageButton.css';
+import React, { Component } from "react";
+import { Pane, Heading } from "evergreen-ui";
+import "../assets/css/ImageButton.css";
+import ImageButton from "./ImageButton";
+
 class Trending extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [
+        {
+          id: "1",
+          img_name: "shoe-1",
+          heading: "Nike Football Impulse Pack",
+        },
+        {
+          id: "1",
+          img_name: "liverpool-1",
+          heading: "Liverpool FC 2021/22 Home Jersey",
+        },
+        {
+          id: "1",
+          img_name: "nike-usa",
+          heading: "The New Us women's National Team Kit",
+        },
+        /* {
+          id: "1",
+          img_name: "nike-usa",
+          heading: "The New Us women's National Team Kit",
+        },*/
+      ],
+    };
+  }
+
   render() {
     return (
-      <Pane>
+      <Pane className="container">
         <Pane>
-          <Heading
-            size={1400}
-            color='#111'
-            marginLeft={20}
-            marginRight={20}
-            paddingTop={10}
-            paddingBottom={20}
-            fontWeight={500}
-            fontFamily='Helvetica Neue|Helvetica|Arial|sans-serif'>
-            Trending
-          </Heading>
-          <Pane
-            display='flex'
-            position='relative'
-            marginLeft={20}
-            marginRight={20}>
-            <Pane className='img-container' marginRight={5}>
-              <img src={liverpool} alt='' />
-              <Pane className='btn-txt'>
-                <Heading size={700} className='txt'>
-                  Liverpool FC 2021/22 Home Jersey
-                </Heading>
-                <Button height={45} className='btn'>
-                  Shop
-                </Button>
-              </Pane>
-            </Pane>
-            <Pane className='img-container' marginLeft={5}>
-              <img src={shoe1} alt='' />
-              <Pane className='btn-txt'>
-                <Heading size={700} className='txt'>
-                  Nike Football Impulse Pack
-                </Heading>
-                <Button height={45} className='btn'>
-                  Shop
-                </Button>
-              </Pane>
-            </Pane>
-          </Pane>
+          <Heading className="header-img">Trending</Heading>
+        </Pane>
+        <Pane className="img-container">
+          {this.state.images.map((image) => (
+            <ImageButton
+              id={image.id}
+              img_name={image.img_name}
+              heading={image.heading}
+            />
+          ))}
         </Pane>
       </Pane>
     );
