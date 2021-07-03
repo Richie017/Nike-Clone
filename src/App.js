594fd3a3db71f8c3137aed198d3b964c6ac56951
Rasheeq Zaman <zamanrshq@gmail.com>
2021-07-03 21:58:43 +0600

    add: preheader.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Topnavbar from './components/header/Topnavbar';
 import Trending from './pages/components/Trending';
 import PreHeader from './components/header/preheader/preheader';
+import Topnavbar from './components/header/Topnavbar';
 import Footer from './components/footer';
 
 class App extends Component {
