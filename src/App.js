997da95a9d8e4f411b3c34e50a947c9c0ac47674
Rasheeq Zaman <zamanrshq@gmail.com>
2021-07-04 01:27:06 +0600

    add: help menu dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Trending from './pages/components/Trending';
-import PreHeader from './components/header/preheader/preheader';
+import PreHeader from './components/header/preheader';
 import Topnavbar from './components/header/Topnavbar';
+import Trending from './pages/components/Trending';
 import Footer from './components/footer';
 
 class App extends Component {
