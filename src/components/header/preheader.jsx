99daceb16480b74de90cccbd931308030304d3fd
Rasheeq Zaman <zamanrshq@gmail.com>
2021-07-03 21:58:43 +0600

    add: preheader.css

--- src/components/header/preheader.jsx
+++ src/components/header/preheader.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { Pane, UnorderedList, ListItem } from 'evergreen-ui';
 
-import jordanLogo from '../../../assets/img/jordan_logo_sm.png';
-import converseLogo from '../../../assets/img/converse_logo_sm.png';
+import jordanLogo from '../../assets/img/jordan_logo_sm.png';
+import converseLogo from '../../assets/img/converse_logo_sm.png';
+
+import '../../assets/css/header/preheader.css';
 
 
 class PreHeader extends Component {
     render() { 
         return (
-            <Pane>
-                <UnorderedList>
+            <Pane className="panePreHeader">
+                <UnorderedList className="iconList">
                     <ListItem><a href="/#"><img src={jordanLogo} alt="Jordan Logo"/></a></ListItem>
                     <ListItem><a href="/#"><img src={converseLogo} alt="Converse Logo"/></a></ListItem>
                 </UnorderedList>
-                <Pane>
+                <Pane className="desktopMenu">
                     <a href="/#">Help</a>
                     <span>|</span>
                     <a href="/#">Join Us</a>
