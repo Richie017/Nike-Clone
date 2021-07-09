e7e6b368e11c9a33d6344161fedeb306f022a13d
Syed Salvi Md Farhavi <salvifaravi111@gmail.com>
2021-07-09 17:27:23 +0600

    update: SignIn complete

--- src/components/header/SignInPopUp.jsx
+++ src/components/header/SignInPopUp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Pane, TextInput, Checkbox } from 'evergreen-ui';
+import { Pane, TextInput, Checkbox, Button } from 'evergreen-ui';
 
 import '../../assets/css/header/signInPopUp.css';
 
@@ -23,9 +23,13 @@ class PopUp extends Component {
     return (
       <Pane className="modal">
         <Pane className="modal_content">
-          {/*<span className="close" onClick={this.handleClick}>
-            &times;
-          </span>*/}
+          <Pane>
+            <Button className="close" onClick={this.handleClick.bind(this)} appearance="none">
+              <span>
+                &times;
+              </span>
+            </Button>
+          </Pane>
           <header>
             <Pane className="nikeImage"></Pane>
             <Pane className="header_text">YOUR ACCOUNT FOR EVERYTHING NIKE</Pane>
@@ -34,12 +38,24 @@ class PopUp extends Component {
             <TextInput className="input_email" placeholder="Email address" />
             <TextInput className="input_password" placeholder="Password" />
           </Pane>
-          <Pane className="test">
-            <Checkbox className="signedIn" checked={this.state.isChecked} label="Keep me signed in" onChange={this.toggleCheck.bind(this)} />
+          <Pane className="signIn_div">
+            <Checkbox className="signedIn_checkbox" checked={this.state.isChecked} label="Keep me signed in" onChange={this.toggleCheck.bind(this)} />
           </Pane>
-          <Pane className="gg">
+          <Pane className="fgot_pass">
             <a href="/#" className="forgot_pass">Forgot password?</a>
           </Pane>
+          <Pane className="policy_text">
+            <p>
+              By logging in, you agree to Nike's <a href="/#">Privacy Policy</a> and <a href="/#">Terms of Use</a>
+              .
+            </p>
+          </Pane>
+          <Pane className="btn">
+            <Button className="btn_signin" appearance="none">
+              Sign In
+            </Button>
+          </Pane>
+          <Pane className="bottom_text">Not a member? <a href="/#" style={{color: "black"}}>Join Us.</a></Pane>
         </Pane>
       </Pane>
     );
