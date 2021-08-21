6a3fe9b3185fdbc70c02fc6343054ffd0f1a57ee
IronManBD <nahidul19967@gmail.com>
2021-08-21 18:49:41 +0600

    add: Men-Women-Kid Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import PreHeader from "./components/header/PreHeader";
 import TopNavBar from "./components/header/TopNavBar";
@@ -7,6 +7,9 @@ import Banner from "./components/header/Banner";
 import Footer from "./components/Footer";
 import HomePageComponents from "./pages/home/model/HomePageComponents";
 import ProductPage from "./pages/products/ProductPage";
+import ManPage from "./pages/ManPage/ManPage";
+import WomenPage from "./pages/WomenPage/WomenPage";
+import KidPage from "./pages/KidPage/KidPage";
 
 class App extends Component {
   render() {
@@ -19,11 +22,25 @@ class App extends Component {
 
           <Switch>
             <Route exact path="/">
-                <HomePageComponents />
-                </Route>
-                <Route exact path="/:category" render={(props) => <ProductPage {...props} /> } />
+              <HomePageComponents />
+            </Route>
 
-               {/* <Route exact path="/:category/:subcategory">
+            <Route exact path="/men">
+              <ManPage />
+            </Route>
+            <Route exact path="/women">
+              <WomenPage />
+            </Route>
+            <Route exact path="/kids">
+              <KidPage />
+            </Route>
+            <Route
+              exact
+              path="/:category"
+              render={(props) => <ProductPage {...props} />}
+            />
+
+            {/* <Route exact path="/:category/:subcategory">
                     <ProductPage />
                 </Route>
                 <Route exact path="/:category/:subcategory/:subsubcategory">
@@ -35,7 +52,6 @@ class App extends Component {
 
           <Footer />
         </Router>
-
       </React.Fragment>
     );
   }
