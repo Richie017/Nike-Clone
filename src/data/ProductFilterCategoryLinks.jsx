6955ae475346b83301a340d04d52a42c2febd2ef
Syed Salvi Md Farhavi <salvifaravi111@gmail.com>
2021-08-29 18:47:38 +0600

    update: hide filter button and side margin

---
+++ src/data/ProductFilterCategoryLinks.jsx
@@ -0,0 +1,15 @@
+export const FilterDataLinks = [
+    {id:"1", title: "Shoes", link: "/#"},
+    {id:"2", title: "Sports Bras", link: "/#"},
+    {id:"3", title: "Bodysuits", link: "/#"},
+    {id:"4", title: "Hoodies & Pullovers", link: "/#"},
+    {id:"5", title: "Jackets & Vests", link: "/#"},
+    {id:"6", title: "Pants & Tights", link: "/#"},
+    {id:"7", title: "Shorts", link: "/#"},
+    {id:"8", title: "Compression & Baselayer", link: "/#"},
+    {id:"9", title: "Tracksuits", link: "/#"},
+    {id:"10", title: "Jumpsuits & Rompers", link: "/#"},
+    {id:"11", title: "Skirts & Dresses", link: "/#"},
+    {id:"12", title: "Socks", link: "/#"},
+    {id:"13", title: "Accessories & Equipment", link: "/#"}
+];
