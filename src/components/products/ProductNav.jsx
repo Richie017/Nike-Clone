6cc69fbfc20a0ac175b4f6008c0c2ce948cf8205
Syed Salvi Md Farhavi <salvifaravi111@gmail.com>
2021-08-29 18:02:44 +0600

    refactor: product page filter option without scroll and color picker

--- src/components/products/ProductNav.jsx
+++ src/components/products/ProductNav.jsx
@@ -1,22 +1,137 @@
 import React, { Component } from "react";
-import { Pane, Radio } from "evergreen-ui";
+import {
+  Pane,
+  UnorderedList,
+  ListItem,
+  Button,
+  CaretUpIcon,
+  CaretDownIcon,
+  PlusIcon,
+  MinusIcon,
+  Checkbox,
+} from "evergreen-ui";
+import "../../assets/css/products/productfilter.css";
+import { FilterDataLinks } from "../../data/ProductFilterCategoryLinks";
+import { FilterData } from "../../data/ProductFilterData";
 
 export default class ProductNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subCategoryLinks: [...FilterDataLinks],
+      productFilterData: [...FilterData]
+    };
+  }
+
+  toggleVisibility(idValue, typeValue) {
+
+    var index = this.state.productFilterData.findIndex((dataId) => dataId.id === idValue);
+    const { productFilterData } = this.state;
+    const copyData = [...productFilterData];
+
+    if (typeValue === "filter") {
+      copyData[index].toggleFilterVisibility = !copyData[index].toggleFilterVisibility;
+      this.setState({
+        productFilterData: copyData,
+      });
+    }
+
+    else if (typeValue === "more") {
+      copyData[index].isMoreClicked = !copyData[index].isMoreClicked;
+
+      if (copyData[index].isMoreClicked === false) {
+        copyData[index].morelessText = "More";
+      }
+
+      else if (copyData[index].isMoreClicked === true) {
+        copyData[index].morelessText = "Less";
+      }
+
+      this.setState({
+        productFilterData: copyData,
+      });
+    }
+  }
+
   render() {
-    const productnavclass = this.props.shownav
-      ? "product-nav"
-      : "product-nonav";
+
     return (
-      <Pane className={productnavclass}>
-        <Pane>
-          <h2>Product search & sort here</h2>
+      <Pane className="product-filter-modal">
+        <Pane className="product-subcateogry-links">
+          <UnorderedList className="subcategory-list-items">
+            {this.state.subCategoryLinks.map((subCategory) => (
+              <ListItem className="filter-list-items" key={subCategory.id}>
+                <a className="filter-list-tags" href={subCategory.link}>
+                  {subCategory.title}
+                </a>
+              </ListItem>
+            ))}
+          </UnorderedList>
         </Pane>
-        <Pane aria-label="Radio Group Label 12" role="group">
-          <Radio name="group" label="Dress" />
-          <Radio name="group" checked label="Pants" />
-          <Radio name="group" disabled label="Shoes" />
-          <Radio checked name="group" label="Undergarments" />
-        </Pane>
+
+        {this.state.productFilterData.map((filterValue) => (
+          <Pane className="filter-view" style={{marginBottom: "0px"}}>
+            <Pane display="flex">
+              <Button
+                className="filter-visibility-btn"
+                key={filterValue.id}
+                onClick={this.toggleVisibility.bind(this, filterValue.id, "filter")}
+                >
+                {filterValue.title}
+              </Button>
+              <Button
+                className="filter-icon-btn"
+                key={filterValue.id}
+                onClick={this.toggleVisibility.bind(this, filterValue.id, "filter")}
+                iconAfter={
+                  filterValue.toggleFilterVisibility === true
+                    ? CaretUpIcon
+                    : CaretDownIcon} 
+              />
+            </Pane>
+
+            {filterValue.toggleFilterVisibility && (
+              <Pane>
+                {filterValue.hasMore === true && filterValue.isMoreClicked === true ?
+                  <Pane>
+                    {filterValue.checkboxValues.map((boxItems) => (
+                      <Pane className="filter-checkbox">
+                        <Checkbox
+                          className="checkbox-items"
+                          label={boxItems.title}
+                          key={boxItems.id}
+                        />
+                      </Pane>
+                    ))}
+                  </Pane>
+                : <Pane>
+                    {filterValue.checkboxValues.slice(0, 4).map((boxItems) => (
+                      <Pane className="filter-checkbox">
+                        <Checkbox
+                          className="checkbox-items"
+                          label={boxItems.title}
+                          key={boxItems.id}
+                        />
+                      </Pane>
+                    ))}
+                  </Pane>
+                }
+
+                {filterValue.hasMore && (
+                  <Pane className="more-visibility-div">
+                    <Button 
+                      className="more-visibility-btn"
+                      onClick={this.toggleVisibility.bind(this, filterValue.id, "more")} 
+                      iconBefore={filterValue.isMoreClicked === true ? MinusIcon : PlusIcon}
+                      >
+                        {filterValue.morelessText}
+                    </Button>
+                  </Pane>
+                )}
+              </Pane>
+            )}
+          </Pane>
+        ))}
       </Pane>
     );
   }
---
+++ src/data/ProductFilterData.jsx
@@ -0,0 +1,159 @@
+export const FilterData = [
+    {
+        id: 1,
+        title: "Gender",
+        toggleFilterVisibility: true,
+        checkboxValues: [
+            {
+                id: 1,
+                title: "Men"   
+            },
+            {
+                id: 2,
+                title: "Women"   
+            },
+            {
+                id: 3,
+                title: "Unisex"   
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Kids",
+        toggleFilterVisibility: true,
+        checkboxValues: [
+            {
+                id: 1,
+                title: "Boys"   
+            },
+            {
+                id: 2,
+                title: "Girls"   
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Shop by Price",
+        toggleFilterVisibility: true,
+        isMoreClicked: false,
+        morelessText: "More",
+        hasMore: true,
+        checkboxValues: [
+            {
+                id: 1,
+                title: "$0 - $25"  
+            },
+            {
+                id: 2,
+                title: "$25 - $50" 
+            },
+            {
+                id: 3,
+                title: "$50 - $100"    
+            },
+            {
+                id: 4,
+                title: "$100 - $150"
+            },
+            {
+                id: 5,
+                title: "Over $150"  
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Sports & Activities",
+        toggleFilterVisibility: true,
+        isMoreClicked: false,
+        morelessText: "More",
+        hasMore: true,
+        checkboxValues: [
+            {
+                id: 1,
+                title: "Lifestyle"  
+            },
+            {
+                id: 2,
+                title: "Running" 
+            },
+            {
+                id: 3,
+                title: "Training & Gym"    
+            },
+            {
+                id: 4,
+                title: "Yoga"
+            },
+            {
+                id: 5,
+                title: "Basketball"  
+            },
+            {
+                id: 6,
+                title: "Football"  
+            },
+            {
+                id: 7,
+                title: "Soccer"  
+            },
+            {
+                id: 8,
+                title: "Golf"  
+            },
+            {
+                id: 9,
+                title: "Skateboarding"  
+            },
+            {
+                id: 10,
+                title: "Tennis"  
+            },
+            {
+                id: 11,
+                title: "Track & Field"  
+            },
+            {
+                id: 12,
+                title: "Walking"  
+            },
+            {
+                id: 13,
+                title: "Volleyball"  
+            },
+            {
+                id: 14,
+                title: "Cheerleading"  
+            },
+            {
+                id: 15,
+                title: "Cycling"  
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "Collaborator",
+        toggleFilterVisibility: true,
+        checkboxValues: [
+            {
+                id: 1,
+                title: "Undercover"   
+            },
+            {
+                id: 2,
+                title: "Gyakusou"   
+            },
+            {
+                id: 3,
+                title: "sacai"   
+            },
+            {
+                id: 4,
+                title: "Off-White"   
+            }
+        ]
+    },
+];
