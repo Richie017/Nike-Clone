cc1dc6fb85a40c4c3938b2401d96b46cd9df5556
Rakib Hossain Ayon <ayon_2009@live.com>
2021-09-01 21:54:44 +0900

    add: onhover effect on products

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -4,30 +4,85 @@ import { NavLink } from "react-router-dom";
 import "../../assets/css/products/product.css";
 
 export default class Product extends Component {
-  render() {
-    const { id, name, type, tag, img, color, price, category, description } =
-      this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      type: this.props.type,
+      tag: this.props.tag,
+      color: this.props.color,
+      price: this.props.price,
+      category: this.props.category,
+      description: this.props.description,
+      showimage: this.props.color[0].img[0],
+      showcolor: false,
+    };
+  }
 
+  render() {
     return (
-      <Pane className="product">
+      <Pane
+        className="product"
+        onMouseEnter={(e) =>
+          this.setState(() => ({
+            showcolor: true,
+          }))
+        }
+        onMouseLeave={(e) =>
+          this.setState(() => ({
+            showcolor: false,
+          }))
+        }
+      >
         <NavLink
           exact
           to={{
-            pathname: "/" + category + "/" + name,
-            state: { productdetails: this.props },
+            pathname: "/" + this.state.category + "/" + this.state.name,
+            state: { productdetails: this.state },
           }}
-          style={{ color: "#000", textDecoration: "none" }}>
+          style={{ color: "#000", textDecoration: "none" }}
+        >
           <img
             className="product-img"
-            src={require("../../pages/assets/images/products/" + img)?.default}
+            src={
+              require("../../pages/assets/images/products/" +
+                this.state.showimage)?.default
+            }
             alt=""
           />
           <Pane>
-            <Pane className="product-tag">{tag ? tag : null}</Pane>
-            <Pane className="product-name">{name}</Pane>
-            <Pane className="product-type">{type}</Pane>
-            {/*<Pane className="product-color">{color} colors</Pane>*/}
-            <Pane className="product-price">${price} </Pane>
+            {this.state.showcolor && this.state.color.length > 1 ? (
+              <Pane>
+                {this.state.color.map((variation) => (
+                  <img
+                    className="product-image-small"
+                    onMouseOver={(e) =>
+                      this.setState(() => ({
+                        showimage: variation.img[0],
+                      }))
+                    }
+                    src={
+                      require("../../pages/assets/images/products/" +
+                        variation.img[0])?.default
+                    }
+                    alt=""
+                  />
+                ))}
+              </Pane>
+            ) : (
+              <Pane>
+                <Pane className="product-tag">
+                  {this.state.tag ? this.state.tag : null}
+                </Pane>
+                <Pane className="product-name">{this.state.name}</Pane>
+                <Pane className="product-type">{this.state.type}</Pane>
+                <Pane className="product-color">
+                  {this.state.color.length} color
+                </Pane>
+              </Pane>
+            )}
+
+            <Pane className="product-price">${this.state.price} </Pane>
           </Pane>
         </NavLink>
       </Pane>
